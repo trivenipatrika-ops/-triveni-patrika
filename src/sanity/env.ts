f819411c1fe8aa63b411c004a74194a7587d5de0
trivenipatrika-ops <trivenipatrika@gmail.com>
2026-08-13 20:45:28 +0530

    Update env.ts

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,8 +1,19 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-01-01";
 
-export const dataset = 
-  process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+export const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
+);
 
-export const projectId = 
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "aihpn19o";
+export const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
+);
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage);
+  }
+  return v;
+}
