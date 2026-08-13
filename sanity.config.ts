b62853a4d85994124fa0d7d9dc7100af73e59d49
trivenipatrika-ops <trivenipatrika@gmail.com>
2026-08-13 13:49:01 +0530

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./src/sanity/schemaTypes";
+import { structure } from "./src/sanity/structure";
 import { apiVersion, dataset, projectId } from "./src/sanity/env";
 
 export default defineConfig({
@@ -9,6 +10,9 @@ export default defineConfig({
   title: "त्रिवेणी पत्रिका — Admin",
   projectId,
   dataset,
-  plugins: [structureTool(), visionTool({ defaultApiVersion: apiVersion })],
-  schema: { types: schemaTypes }
+  plugins: [
+    structureTool({ structure }),
+    visionTool({ defaultApiVersion: apiVersion }),
+  ],
+  schema: { types: schemaTypes },
 });
