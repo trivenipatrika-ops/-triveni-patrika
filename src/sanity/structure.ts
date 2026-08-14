1996e0ad5817e862ead4ae6560a62c2bc685acad
trivenipatrika-ops <trivenipatrika@gmail.com>
2026-08-14 10:23:23 +0530

    Update structure.ts

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -2,9 +2,11 @@ import type { StructureResolver } from "sanity/structure";
 
 export const structure: StructureResolver = (S) =>
   S.list()
+    .id("root")
     .title("त्रिवेणी पत्रिका")
     .items([
       S.listItem()
+        .id("posts")
         .title("✍️ सभी खबरें")
         .child(
           S.documentTypeList("post")
@@ -12,9 +14,11 @@ export const structure: StructureResolver = (S) =>
             .defaultOrdering([{ field: "publishedAt", direction: "desc" }])
         ),
       S.listItem()
+        .id("categories")
         .title("🏷️ श्रेणियाँ")
         .child(S.documentTypeList("category").title("श्रेणियाँ")),
       S.listItem()
+        .id("authors")
         .title("✒️ लेखक/रिपोर्टर")
         .child(S.documentTypeList("author").title("लेखक/रिपोर्टर")),
     ]);
